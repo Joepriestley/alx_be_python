30d27e23794a9b681bd29b7bc0f7180b074b54c4
Joepriestley <joeamonoo1114@gmail.com>
2025-05-26 21:52:45 +0100

    Add content to python_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+
+number2 = 5
+
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+
+print("Subtractin of" + " " + str(number1) + " " + "and" + " " + str(number2) + " "+ "is" + str(number1 - number2))
+
+print(f"Multiplication of {number1} and {number2} is {number1*number2}")
