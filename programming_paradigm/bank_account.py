0f87d1863bc569110fd0b40b7435470c2b937e5e
Joepriestley <joeamonoo1114@gmail.com>
2025-06-17 21:40:45 +0100

    corrected mistake in bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -19,7 +19,7 @@ class BankAccount:
         
 
     def display_balance(self):
-        return (f"Current balance: ${self.account_balance}")
+        return (f"Current Balance: ${self.account_balance}")
     
     
 account= BankAccount()
