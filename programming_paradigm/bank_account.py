8c2371b08dc4a10b4861c0af44b002ece490a496
Joepriestley <joeamonoo1114@gmail.com>
2025-06-17 21:56:35 +0100

    changed to added floating point  instance in bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,7 +4,7 @@ class BankAccount:
     
     def __init__(self,account_balance = 0.0):
         
-        self.account_balance = float(account_balance)
+        self.account_balance = account_balance
         
     def deposit(self, amount):
         newAmount = self.account_balance =+ amount 
@@ -12,11 +12,11 @@ class BankAccount:
 
         
     def withdraw(self,amount):
-        if self.account_balance -float(amount) >= 0:
+        if self.account_balance -amount >= 0:
             return True
         
 
     def display_balance(self):
-        return (f"Current Balance: ${self.account_balance}")
+        return (f"Current Balance: ${self.account_balance:.2f}")
     
     
