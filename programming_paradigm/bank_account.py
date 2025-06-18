ad134413de0ad3d43374d62df8bc9957279938e9
Joepriestley <joeamonoo1114@gmail.com>
2025-06-18 21:54:50 +0100

    corrected in bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,24 +14,14 @@ class BankAccount:
 
         
     def withdraw(self,amount):
+        
         if amount > self.account_balance:
             return f"Insufficient funds. Your account balance is: ${self.account_balance}"
-        if amount <= self.account_balance:
-            self.account_balance -=amount
-            return f"${amount} withdrawn. Current Balance: ${self.account_balance} "
+
+        self.account_balance -=amount
+        return f"${amount} withdrawn. Current Balance: ${self.account_balance} "
         
 
     def display_balance(self):
         return (f"Current Balance: ${self.account_balance}")
-
-# joe_account =BankAccount()
-# joe_account.deposit(70000)
-# print(joe_account.withdraw(8000))
-# print(joe_account.display_balance())
-# account =BankAccount()
-# account.deposit(3000)
-# account.deposit(500)
-# print(account.withdraw(300))
-# print(account.display_balance())
-
 
