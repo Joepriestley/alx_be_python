0e0de17d107e53e31cce51caa3d64ec20af5aa83
Joepriestley <joeamonoo1114@gmail.com>
2025-06-21 04:58:47 +0100

    add

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,27 +1,20 @@
 class BankAccount:
-    """class representing a bank account
-    """
-    
-    def __init__(self,account_balance = 0):
-        
+    """Class representing a bank account."""
+
+    def __init__(self, account_balance: float = 0.0):
         self.account_balance = account_balance
-        
-    def deposit(self, amount):
-        if amount <=0:
-            return f"Amount must be positive"
-        self.account_balance += amount 
+
+    def deposit(self, amount: float) -> float:
+        if amount <= 0:
+            raise ValueError("Amount must be positive")
+        self.account_balance += amount
         return self.account_balance
 
-        
-    def withdraw(self,amount):
-        
+    def withdraw(self, amount: float) -> bool:
         if amount > self.account_balance:
             return False
-
-        self.account_balance -=amount
+        self.account_balance -= amount
         return True
-        
 
-    def display_balance(self):
-        return (f"Current Balance: ${self.account_balance:.2f}")
-
+    def display_balance(self) -> str:
+        return f"Current Balance: ${self.account_balance:.2f}"
