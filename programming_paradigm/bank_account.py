9254e0d2e34dca8ee80082d8754607ce472768cd
Joepriestley <joeamonoo1114@gmail.com>
2025-06-21 03:57:20 +0100

    add decimals to account balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -23,5 +23,5 @@ class BankAccount:
         
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        return (f"Current Balance: ${self.account_balance:.2f}")
 
